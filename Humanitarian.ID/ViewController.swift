c4e93a264bde61e7199dc216190bf5265ff8c42a
Hayyaan Ahmad <hayyaan@gmail.com>
2015-11-07 03:16:03 +0500

    Initial Commit

---
+++ Humanitarian.ID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Humanitarian.ID
+//
+//  Created by Hayyaan Ahmad on 11/7/15.
+//  Copyright © 2015 UN OCHA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
