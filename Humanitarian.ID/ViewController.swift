25a8d825e563a2299a27dee2b761f00f1a033d9c
Hayyaan Ahmad <hayyaan@gmail.com>
2015-12-02 01:34:32 +0500

    initial commit

--- Humanitarian.ID/ViewController.swift
+++ Humanitarian.ID/ViewController.swift
@@ -7,17 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    @IBOutlet var containerView : UIView?
+    var webView : WKWebView!
+    
+    let url = NSURL(string: "https://humanitarian.id")
+    
+    override func loadView() {
+        super.loadView()
+        self.webView = WKWebView(
+            frame: CGRectOffset(self.view.frame, 0, 20)
+        )
+//        self.view.frame = CGRectOffset(self.view.frame, 0, 20)
+        self.view = self.webView
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let request = NSURLRequest(URL: url!)
+        
+        
+        self.webView.loadRequest(request)
+        self.webView.allowsBackForwardNavigationGestures = true
+        
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
 
